e6a34cc5e06f2f443e997b9e55ac1505f406d85b
DELL <ferhatagca@gmail.com>
2022-08-01 23:29:56 +0300

    revised1

---
+++ src/Gun14/FridayProject_3_Hepsi_Ferhat.java
@@ -0,0 +1,13 @@
+package Gun14;
+
+import Utils.BasicStaticDriver;
+
+public class FridayProject_3_Hepsi_Ferhat extends BasicStaticDriver {
+    public static void main(String[] args) {
+
+
+
+
+
+    }
+}
